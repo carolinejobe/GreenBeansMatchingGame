e7eb20bc39e5587a44e9785da8a797531cd05d76
carolinejobe <caroline.h.jobe@gmail.com>
2016-02-12 11:15:27 -0500

    add randomizer class back

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Randomizer {
 
-
-	ArrayList<card> cardList = ArrayList<card>;
+	//this class will randomize the 16 card objects and assign them to the array of JToggle buttons in the Gui
+	ArrayList<cards> cardList = ArrayList<cards>;
 	
 	Collections.shuffle(cardList);
 	
