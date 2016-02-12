e6575e26ab8550afc416d7a2612eb491b92301ad
carolinejobe <caroline.h.jobe@gmail.com>
2016-02-12 10:33:36 -0500

    adding randomizer class

---
+++ src/Randomizer.java
@@ -0,0 +1,15 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Randomizer {
+
+
+	ArrayList<card> cardList = ArrayList<card>;
+	
+	Collections.shuffle(cardList);
+	
+	return cardList;
+		
+
+}
