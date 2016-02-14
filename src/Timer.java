8d5f7ff4f670a7d08afb732f9859e83bf34a60c1
neowalla <neowalla@gmail.com>
2016-02-13 19:44:31 -0500

    timer with a new line or two

--- src/Timer.java
+++ src/Timer.java
@@ -1,20 +1,24 @@
 import java.util.Date;
 import java.util.TimerTask;
 
-
 public class Timer {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	
 
-		
 		long startTime = System.currentTimeMillis();
-		long elapsedTime = 0L;
+		long elapsedTime = System.currentTimeMillis() - startTime;
+		long elapsedSeconds = elapsedTime / 1000;
+		long secondsDisplay = elapsedSeconds % 60;
+		long elapsedMinutes = elapsedSeconds / 60;
+		// put here code to format and display the values
 
-		while (elapsedTime < 2*60*1000) {
-		    elapsedTime = (new Date()).getTime() - startTime;
+		//while (elapsedTime < 2 * 60 * 1000) {
+			//elapsedTime = (new Date()).getTime() - startTime;
+
+			void timer() {
+			System.out.println(startTime);
 		}
 	}
-}
